5668e37f79b84be1ecce6a062d38776e94853fb2
ivanb <bileichuk.ivan@gmail.com>
2024-11-21 15:41:24 +0200

    Added Mission & Target models to missions/models.py

--- missions/models.py
+++ missions/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from cats.models import Cat
 
-# Create your models here.
+
+class Mission(models.Model):
+    cat = models.ForeignKey(
+        Cat, related_name="missions", on_delete=models.CASCADE
+    )
+    is_complete = models.BooleanField(default=False)
+    targets = models.ManyToManyField(
+        "Target", related_name="missions", blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+
+class Target(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    notes = models.TextField()
+    is_complete = models.BooleanField(default=False)
