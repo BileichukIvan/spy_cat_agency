bbb265d7368a090d8af0c2a4eb0c14fd1ef10207
ivanb <bileichuk.ivan@gmail.com>
2024-11-21 16:07:08 +0200

    Added CatViewsSet with 'Update only salary restriction' to cats.views.py

--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from cats.models import Cat
+from cats.serializers import CatSerializer
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        allowed_updates = {"salary"}
+        if any(key not in allowed_updates for key in request.data.keys()):
+            return Response(
+                {"error": "You can update only salary field"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        self.perform_update(serializer)
+        return Response(serializer.data)
